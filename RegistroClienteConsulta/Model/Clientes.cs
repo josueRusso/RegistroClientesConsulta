ecf58db63e4dbe8dc32f4237e605e8594340edfa
josueRusso <josuerusso1911@gmail.com>
2023-05-25 21:31:52 -0400

    Actualizando a string los atributos Issues #4

--- RegistroClienteConsulta/Model/Clientes.cs
+++ RegistroClienteConsulta/Model/Clientes.cs
@@ -10,15 +10,15 @@ namespace RegistroClienteConsulta.Model
 
         [Required(ErrorMessage = "Es obligatorio introducir el nombre del cliente")]
         public string? Nombre { get; set;}
-        public double Telefono { get; set; }
+        public string? Telefono { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir la cédula del cliente")]
         [Range(minimum: 11, maximum: 11, ErrorMessage = "Debe estar en un rango permitido")]
-        public double Cedula { get; set; }
+        public string? Cedula { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir el RNC del cliente")]
         [Range(minimum: 9, maximum: 9, ErrorMessage = "Debe estar en un rango permitido")]
-        public double Rnc { get; set; }
+        public string? Rnc { get; set; }
         public string? Email { get; set; }
         public string? Direccion { get; set; }
     }
