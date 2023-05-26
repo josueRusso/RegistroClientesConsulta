b0b4dd4013e3879534987628f0f9e07b1da15bdf
josueRusso <josuerusso1911@gmail.com>
2023-05-25 21:39:44 -0400

    Actualizando la condicion debe ser || no && Issues #8

--- RegistroClienteConsulta/BLL/ClientesBLL.cs
+++ RegistroClienteConsulta/BLL/ClientesBLL.cs
@@ -9,19 +9,24 @@ using RegistroClienteConsulta.DAL;
 using RegistroClienteConsulta.Model;
 using System.Diagnostics.Contracts;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace RegistroClienteConsulta.BLL
 {
     public class ClientesBLL
     {
+
         private Context _context;
+
         public ClientesBLL(Context Context)
         {
             _context = Context;
         }
+
         public bool Existe(int ClienteId)
         {
             return _context.Cliente.Any(s => s.ClienteId == ClienteId);
         }
+
         private bool Insertar(Clientes cliente)
         {
             _context.Cliente.Add(cliente);
@@ -35,6 +40,7 @@ namespace RegistroClienteConsulta.BLL
             int verificar = _context.SaveChanges();
             return verificar > 0;
         }
+
         public bool Guardar(Clientes cliente)
         {
             if (!Existe(cliente.ClienteId))
@@ -42,18 +48,21 @@ namespace RegistroClienteConsulta.BLL
             else
                 return Modificar(cliente);
         }
+
         public bool Eliminar(Clientes cliente)
         {
             _context.Cliente.Remove(cliente);
             int verificar = _context.SaveChanges();
             return verificar > 0;
         }
+
         public Clientes? Buscar(int ClienteId)
         {
             return _context.Cliente
                 .AsNoTracking()
                 .FirstOrDefault(s => s.ClienteId == ClienteId);
         }
+
         public List<Clientes> GetList(Expression<Func<Clientes, bool>> Criterio)
         {
             return _context.Cliente
@@ -61,23 +70,21 @@ namespace RegistroClienteConsulta.BLL
                 .AsNoTracking()
                 .ToList();
         }
-        public bool Validar(string? nombre, double Rnc)
+
+        public bool Validar(string nombre, string Rnc)
         {
 
             bool confirmar = false;
             try
             {
-                confirmar = _context.Cliente.Any(e => e.Nombre.ToLower() == nombre.ToLower()  && e.Rnc == Rnc );
-
+                confirmar = _context.Cliente.Any(e => e.Nombre.ToLower() == nombre.ToLower()  || e.Rnc == Rnc );
             }
             catch (Exception)
             {
                 throw;
             }
-
             return confirmar;
         }
-
     }
 }
 
