b8d7873078ff4c431dfa30cffed21e765da7a326
josueRusso <josuerusso1911@gmail.com>
2023-05-28 06:34:19 -0400

    Creando la Model

--- RegistroClienteConsulta/Model/Tickets.cs
+++ RegistroClienteConsulta/Model/Tickets.cs
@@ -1,6 +1,28 @@
-﻿namespace RegistroClienteConsulta.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroClienteConsulta.Model
 {
     public class Tickets
     {
+        [Key]
+        public int TicketIdId { get; set; }
+
+        [Range(minimum: 0, maximum: 50, ErrorMessage = "Debe estar en un rango permitido")]
+        public int Fecha { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public int SistemaId { get; set; }
+
+        public int PrioridadId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el por quien fue solicitado")]
+        public string? SolicitadoPor { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir Un Asunto")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir una Descripcion")]
+        public string? Descripcion { get; set; }
     }
 }
