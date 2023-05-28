394933260fc008fda6495380ed76e0097cce94d1
josueRusso <josuerusso1911@gmail.com>
2023-05-28 12:34:32 -0400

    Crando la model Sistema

---
+++ RegistroClienteConsulta/Model/Sistemas.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroClienteConsulta.Model
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+
+        [Required (ErrorMessage ="El nombre del sistema es obligatorio")]
+        public String Nombre { get; set; }
+    }
+}
