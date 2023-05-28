3884a30a4e83cf91f926c27db1587b7119fab3ed
josueRusso <josuerusso1911@gmail.com>
2023-05-28 12:40:58 -0400

    Crando SistemasBLL

--- RegistroClienteConsulta/DAL/Context.cs
+++ RegistroClienteConsulta/DAL/Context.cs
@@ -10,7 +10,9 @@ namespace RegistroClienteConsulta.DAL
 
         public DbSet<Tickets> Tickets { get; set; }
 
-        public DbSet<Prioridades> Prioridad { get; set; }   
+        public DbSet<Prioridades> Prioridad { get; set; }
+
+        public DbSet<Sistemas>Sistemas { get; set; }
 
         public Context(DbContextOptions<Context> options) : base(options) { }
 
