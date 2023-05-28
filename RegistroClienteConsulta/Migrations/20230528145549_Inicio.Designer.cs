b608f3f0af417e8b36396f002dd4531da02e5fa1
josueRusso <josuerusso1911@gmail.com>
2023-05-28 10:56:58 -0400

    Actualizando El validar

--- RegistroClienteConsulta/Migrations/20230528145549_Inicio.Designer.cs
+++ RegistroClienteConsulta/Migrations/20230528145549_Inicio.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroClienteConsulta.DAL;
 
@@ -10,9 +11,11 @@ using RegistroClienteConsulta.DAL;
 namespace RegistroClienteConsulta.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230528145549_Inicio")]
+    partial class Inicio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
